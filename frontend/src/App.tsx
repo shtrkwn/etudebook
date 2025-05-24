0dd463bb4b5a675ce7f7a6b5e23c412123c593b7
Shotaro Kawano <51527477+shtrkwn@users.noreply.github.com>
2025-05-24 14:29:39 +0900

    Add Material UI and update pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CssBaseline, AppBar, Toolbar, Typography, Container } from '@mui/material';
+import Home from './pages/Home';
+import Play from './pages/Play';
 
-const App: React.FC = () => {
-  return <div>Hello from React frontend</div>;
-};
+const App: React.FC = () => (
+  <BrowserRouter>
+    <CssBaseline />
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          Etudebook
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container sx={{ mt: 2 }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/play/:songId" element={<Play />} />
+      </Routes>
+    </Container>
+  </BrowserRouter>
+);
 
 export default App;
